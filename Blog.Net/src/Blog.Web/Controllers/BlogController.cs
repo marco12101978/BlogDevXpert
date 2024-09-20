a47a4bb611a8320efa194111586c1d5997d7119b
Marco Imperium <marco@imperiumsolucoes.com.br>
2024-09-20 16:45:32 -0300

    Start -> Controllers e Views

---
+++ Blog.Net/src/Blog.Web/Controllers/BlogController.cs
@@ -0,0 +1,22 @@
+﻿using Blog.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BlogController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var posts = await _context.Posts.Include(p => p.Autor).ToListAsync();
+            return View(posts);
+        }
+    }
+}
---
+++ Blog.Net/src/Blog.Web/Controllers/PostagemController.cs
@@ -0,0 +1,45 @@
+﻿using Blog.Data.Models;
+using Blog.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Controllers
+{
+    public class PostagemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostagemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Authors = _context.Authors.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Postagem post)
+        {
+            if (ModelState.IsValid)
+            {
+                post.DataCriacao = DateTime.Now; 
+                _context.Add(post);
+                await _context.SaveChangesAsync(); 
+                return RedirectToAction("Index");
+            }
+            return View(post); 
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var posts = await _context.Posts.Include(p => p.Autor).ToListAsync();
+            return View(posts);
+        }
+    }
+}
